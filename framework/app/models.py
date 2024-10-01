3c429eb9d07307c7d67b8edf069c5796634b1574
PirateCoderX <PirateCoderX@gmail.com>
2024-10-01 14:45:24 +0330

    impelementing database and custom register

--- framework/app/models.py
+++ framework/app/models.py
@@ -1,3 +1,63 @@
-class User:
-    pass
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Date
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+from sqlalchemy.orm import Session
+from app.migrations import engine
+from bcrypt import hashpw, gensalt
+from werkzeug.security import check_password_hash
+from sqlalchemy.exc import IntegrityError
 
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), nullable=False, unique=True)
+    password = Column(String(20), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+    last_login = Column(DateTime, default=datetime.datetime.now())
+
+    post = relationship("Post", back_populates="author", cascade="all, delete-orphan")
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    create_at = Column(DateTime, default=datetime.datetime.now())
+    title = Column(String(100), nullable=False)
+    body = Column(String, nullable=False)
+
+    author = relationship("User", back_populates="post")
+
+class Session(Base):
+    __tablename__ = 'sessions'
+    
+    session_id = Column(String(100), primary_key=True)
+    session_data = Column(String, nullable=False)
+    expire_date = Column(Date, nullable=False)
+
+
+def custom_register(data):
+    if data["password1"] != data["password2"] or len(data["password1"]) < 6:
+        return False
+    
+    hashed_password = hashpw(data["password1"].encode('utf-8'), gensalt())
+
+    user = User(
+        username=data["username"],
+        password=hashed_password,
+        email=data["email"]
+    )
+
+    with Session(engine) as session:
+        session.add(user)
+        try:
+            session.commit()
+            return True
+        except IntegrityError:
+            session.rollback()
+            return False
--- framework/app/views.py
+++ framework/app/views.py
@@ -1,9 +1,108 @@
 from core.http import render_template
+from app.models import custom_register
 
-def index(request):
-    context = {'title': 'Home', 'message': 'Welcome to the Home Page'}
-    return render_template('index.html', context)
+def login(request):
+    context={'title':'welcome to the DevConnector',
+             'sign':'Sign In',
+             'text':'Sign into your account',
+             'text2':'Dont have an account ?'
+    }
+    return render_template('login.html', context)
 
-def about(request):
-    context = {'title': 'About', 'message': 'This is the About Page'}
-    return render_template('about.html', context)
+def post(request):
+    context = {
+        'title': 'Welcome To The Developer Connector',
+        'main_post': {
+            'avatar_url': 'image',
+            'author': 'name',
+            'content': 'Lorem'
+        },
+        'comments': [
+            {
+                'avatar_url': 'image',
+                'author': 'name',
+                'content': 'Lorem'
+            }
+        ]
+    }
+    return render_template('post.html', context)
+
+def posts(request):
+    context = {
+        'title': 'Welcome To The Developer Connector',
+        'comments': [
+            {
+                'avatar_url': 'image',
+                'author': 'name',
+                'content': 'Lorem'
+            }
+        ]
+    }
+    return render_template('posts.html', context)
+
+def register(request):
+    data = request.POST
+
+    if request.command=='POST' and custom_register(data):
+        posts(request)
+    
+    elif request.command=='GET':
+        context = {
+            'title': 'Welcome To The Developer Connector',
+            'header' : 'Sign Up',
+            'paragrapgh':'Create Your Account',
+            'paragraph2':'Already have an account?'
+
+        }
+        return render_template('register.html', context)
+
+
+# def index(request):
+#     context = {'title': 'Welcome to the DevConnector',
+#                'heading': 'Developer Connector',
+#                'description': 'Create developer profile/portfolio, share posts and get help from other developers'
+#                }
+#     return render_template('index.html', context)
+
+# def add_education(request):
+#     context={'title':'welcome to the DevConnector',
+#              'text_header':'Add Your Education',
+#              'description':' Add any school, bootcamp, etc that you have attended',
+#              'date1':'From Date',
+#              'date2':'To Date',
+#              'paragraph': 'Current School'
+#     }
+#     return render_template('add-education.html', context)
+
+# def add_experience(request):
+#     context={'title':'welcome to the DevConnector',
+#              'text_header':'Add An Experience',
+#              'description':' Add any developer/programming positions that you have had in the past',
+#              'date1':'From Date',
+#              'date2':'To Date',
+#              'paragraph': 'Current job'
+#     }
+#     return render_template('add-experience.html', context)
+
+# def create_profile(request):
+#     context={'title':'welcome to the DevConnector',
+#              'text_header':'Create Your Profile',
+#              'description': "Let's get some information to make your profile stand out",
+#     }
+#     return render_template('create-profile.html', context)
+
+# def dashboard(request):
+    
+#     context = {
+#         'title': 'Dashboard - DevConnector',
+#         'dashboard_title': 'Dashboard',
+#         'user_name': 'John Doe',
+#         'experiences': [
+#             {'company': 'Microsoft', 'title': 'Senior Developer', 'years': 'Oct 2011 - Current'},
+#             {'company': 'Sun Microsystems', 'title': 'Senior Developer', 'years': 'Oct 2004 - Nov 2010'}
+#         ],
+#         'educations': [
+#             {'school': 'University of Washington', 'degree': 'Masters', 'years': 'Sep 1993 - June 1999'}
+#         ]
+#     }
+#     return render_template('dashboard.html', context)
